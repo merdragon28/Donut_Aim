10c7ba72573b7c8a13ba31de09d8b2cc9c4b6383
merdragon28 <54519958+merdragon28@users.noreply.github.com>
2020-02-29 19:05:24 -0800

    Added Class Requirements Functionality
    1. Mouse support added but buggy
    2. Controller Bug Fixed
    3. Start and Quit functionality added along with start scene
    4. Added a line that gets bigger as camera starts moving faster.
    
    Other:
    Changed fire button to alter color of cube material.
    
    Made a lot of code into functions and seperated out dependancies within the class to make adjustments easier to make and sequencing less important.
    
    Changed the deadzone to be dependent on input rather than angle. Change makes finding deadzone start point easier.
    
    upon camera unlocking will now start slowly accelerating instead of instantly starting at the speed it'd be at if there was no dead zone.
    
    Should be configured to make a working build with start and exit functionality as well as automatic transitions between mouse and controller if controller is turned off or on.
    
    Removed Player movement code as it was not immediately part of the project.

--- Donut_Aim/Assets/ControllerInput.cs
+++ Donut_Aim/Assets/ControllerInput.cs
@@ -7,35 +7,28 @@ public class ControllerInput : MonoBehaviour
 {
     //creates a line renderer and defines its width and length
     public LineRenderer laserLineRenderer;
-    public float laserWidth = 0.1f;
+    public float lineStartWidth;
+    public float lineMaxEndWidth;
     public float laserMaxLength = 1000f;
     //defines how fast the player rotates the camera and moves in game
-    private int moveSpeed = 4;
     private int rotationSpeed = 2;
-    //defines how far to draw debug ray
-    int rayDistance = 100;
-
     //determines whether the camera can move or not
     public bool camlock = false;
-
     //the camera attachedto the player
     public Camera cam;
-
     //Reticle
     public GameObject reticle;
     public Material retmat;
-
     //MovementLimitRing
     public GameObject ring;
     public float ringSize = 5;
-
-    //Rays
-    Ray ray;
-    RaycastHit raycastHit;
-
     //dead angle (if vector is less than that angle camera will not move)
-    public float deadzone = 10f;
-
+    public float deadzone;
+    //direction player is aiming in
+    Vector3 aimDirection;
+    //necessary rays
+    Ray forwardRay;
+    Ray aimDirectionRay;
     void Start()
     {
         //ring.transform.localScale = new Vector3(ringSize, ringSize, ringSize);
@@ -45,51 +38,109 @@ public class ControllerInput : MonoBehaviour
         //Modified code from internet that sets Line render properties
         Vector3[] initLaserPositions = new Vector3[2] { Vector3.zero, Vector3.zero };
         laserLineRenderer.SetPositions(initLaserPositions);
-        laserLineRenderer.startWidth = laserWidth;
-        laserLineRenderer.endWidth = laserWidth;
+        laserLineRenderer.startWidth = lineStartWidth;
+        laserLineRenderer.endWidth = lineMaxEndWidth;
+
+        aimDirection = Vector3.zero;
+
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
         //scales rings size in game
         ring.transform.localScale = new Vector3(ringSize, ringSize, ringSize);
 
+        bool fire = false;
+        bool quit = false;
+        Vector2 aimInput = Vector2.zero;
         //finds and sets gamepad
-        var gamepad = Gamepad.current;
-        if (gamepad == null)
-            return; // No gamepad connected.
-
-        //allows the player to straiff left and right from their current position and move forward or backward in the direction of the camera.
-        Vector2 move = gamepad.leftStick.ReadValue();
-        transform.position += move.y * transform.forward * Time.deltaTime * moveSpeed;
-        transform.position += move.x * transform.right * Time.deltaTime * moveSpeed;
-
-        //Draws aiming Direction
-        Vector2 aim = gamepad.rightStick.ReadValue();
-        float fov = cam.fieldOfView;
-        float aspect = cam.aspect;
-        Vector3 aimDirection = transform.forward + (transform.up * aim.y * (fov / 110)) + (transform.right * aim.x * (fov / 110) * aspect);
-        Debug.DrawRay(transform.position, aimDirection * rayDistance, Color.green);
-
-        if (deadzone > Vector3.Angle(aimDirection, transform.forward))
+        if(Gamepad.current != null)
         {
-            camlock = true;
+            var gamepad = Gamepad.current;
+            aimInput = gamepad.rightStick.ReadValue();
+            fire = gamepad.rightTrigger.isPressed;
+            quit = gamepad.startButton.isPressed;
         }
-        else
+        //use mouse
+        else if(Input.mousePresent)
         {
-            camlock = false;
+            aimInput = new Vector2((Input.mousePosition.x-(Screen.width/2))/(Screen.width/2),(Input.mousePosition.y-(Screen.height/2)) /(Screen.height/2));
+            Debug.Log(aimInput);
+            fire = Input.GetMouseButtonDown(1);
+            quit = Input.GetKey(KeyCode.Escape);
+            if (aimInput.x > 1)
+                aimInput.x = 1;
+            else if (aimInput.x < -1)
+                aimInput.x = -1;
+            if (aimInput.y > 1)
+                aimInput.y = 1;
+            else if (aimInput.y < -1)
+                aimInput.y = -1;
         }
 
-        //allows player to aim the camera
-        if (camlock == false)
+        if(quit)
         {
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-1 * aim.y * rotationSpeed, aim.x * rotationSpeed, 0));
+            Application.Quit();
         }
+
+        //sets aiming Direction
+        SetAimDirection(aimInput);
+
+        forwardRay = new Ray(transform.position, transform.forward);
+        aimDirectionRay = new Ray(transform.position, aimDirection);
+
+        MoveCamera(aimInput, aimDirection);
 
         //makes the reticle to follow thet aim vector and changes its color when it collides
-        ray = new Ray(transform.position, aimDirection);
-        if (Physics.Raycast(ray, out raycastHit, laserMaxLength))
+        MoveReticle(aimDirection, aimDirectionRay);
+
+        //draws a line from center to aim reticle
+        //ShootLaserFromTargetPosition(transform.forward*25, aimDirection, laserMaxLength);
+        
+        //feedback line
+        SetLineRenderPoints(forwardRay.GetPoint(25), aimDirectionRay.GetPoint(25),5, aimInput);
+
+        //Fire
+        if (fire)
+        {
+            FireAction(aimDirectionRay);
+        }
+
+        //caues camera aim ring to follow cammera
+        FollowCamera(aimInput, forwardRay);
+    }
+
+    void FollowCamera(Vector2 aimInput, Ray forwardRay)
+    {
+        forwardRay = new Ray(transform.position, transform.forward);
+        ring.transform.position = forwardRay.GetPoint(25);
+        ring.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-1 * aimInput.y * rotationSpeed, aimInput.x * rotationSpeed, 0));
+    }
+
+    void SetLineRenderPoints(Vector3 start, Vector3 end, float deadzone, Vector2 aimInput)
+    {
+        //Debug.Log("Start: " + start + " End: " + end);
+        if (Vector3.Distance(start, end) > deadzone)
+        {
+            Ray moveTowards = new Ray(start, (end - start).normalized);
+            start = moveTowards.GetPoint(deadzone);
+        }
+        else
+        { start = end; }
+        laserLineRenderer.SetPosition(0, start);
+        laserLineRenderer.SetPosition(1, end);
+        laserLineRenderer.endWidth = aimInput.magnitude * lineMaxEndWidth;
+    }
+
+    void MoveReticle(Vector3 aimDirection, Ray aimingRay)
+    {
+        RaycastHit hit;
+        //makes the reticle to follow thet aim vector and changes its color when it collides
+        aimingRay = new Ray(transform.position, aimDirection);
+        if (Physics.Raycast(aimingRay, out hit, laserMaxLength))
         {
             retmat.color = Color.green;
         }
@@ -97,25 +148,40 @@ public class ControllerInput : MonoBehaviour
         {
             retmat.color = Color.red;
         }
-        reticle.transform.position = ray.GetPoint(25);
-
-        //ShootLaserFromTargetPosition(transform.position - new Vector3(0, 0.1f, 0), aimDirection, laserMaxLength);
-
-        //Fire
-        if (gamepad.rightTrigger.isPressed)
-        {
-            if (Physics.Raycast(ray, out raycastHit, laserMaxLength))
-            {
-                Object thing = raycastHit.collider.gameObject;
-                Destroy(thing);
-            }
-        }
-
-        //caues camera aim ring to follow cammera
-        ray = new Ray(transform.position, transform.forward);
-        ring.transform.position = ray.GetPoint(25);
-        ring.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-1 * aim.y * rotationSpeed, aim.x * rotationSpeed, 0));
+        reticle.transform.position = aimingRay.GetPoint(25);
     }
+
+    void SetAimDirection(Vector2 aimInput)
+    {
+        float fov = cam.fieldOfView;
+        float aspect = cam.aspect;
+        aimDirection = transform.forward + (transform.up * aimInput.y * (fov / 110)) + (transform.right * aimInput.x * (fov / 110) * aspect);
+    }
+
+    void MoveCamera(Vector2 aimInput, Vector3 aimDirection)
+    {
+        Debug.Log(aimInput.magnitude);
+    //locks camera if in deadzone
+    camlock = deadzone > aimInput.magnitude;
+        //allows player to aim the camera
+        if (camlock == false)
+        {
+            Vector2 startvelocity = aimInput;
+            startvelocity = startvelocity.normalized*0.3f;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-1 * (aimInput.y-startvelocity.y)* rotationSpeed, (aimInput.x-startvelocity.x)* rotationSpeed, 0));
+        }
+    }
+
+    void FireAction(Ray aimingRay)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(aimingRay, out hit, laserMaxLength))
+        {
+            Debug.Log(hit.collider.GetComponent<MeshRenderer>().sharedMaterials[0]);
+            hit.collider.GetComponent<MeshRenderer>().sharedMaterials[0].color = new Color (Random.value,Random.value,Random.value);
+        }
+    }
+
     //copied from internet for drawing raycast
     void ShootLaserFromTargetPosition(Vector3 targetPosition, Vector3 direction, float length)
     {
